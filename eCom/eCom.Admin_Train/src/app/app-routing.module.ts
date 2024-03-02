cf0d63f68137747119937efc8e7206cf06ae81e1
tamdao <tam.dao@bnksolution.com>
2024-03-02 21:25:29 +0700

    add prj admin_tran

--- eCom/eCom.Admin_Train/src/app/app-routing.module.ts
+++ eCom/eCom.Admin_Train/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
