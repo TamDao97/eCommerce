6ad9febee39feb8d11bb7465f33491f29462037b
tamdao <tam.dao@bnksolution.com>
2024-03-03 00:36:06 +0700

    update

---
+++ eCom/eCom.Admin_Train/src/app/layout/layout.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, NavComponent],
+  imports: [CommonModule],
+  exports: [NavComponent, HeaderComponent, FooterComponent],
+})
+export class LayoutModule {}
